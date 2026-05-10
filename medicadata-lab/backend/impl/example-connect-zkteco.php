1d7e58bf5a01dbef37d9e52086f0f9a977a3f2d7
Joan Dariel Sánchez Gallegos <joangallegos39@gmail.com>
2026-05-09 22:10:41 -0600

    Update example-connect-zkteco.php

--- medicadata-lab/backend/impl/example-connect-zkteco.php
+++ medicadata-lab/backend/impl/example-connect-zkteco.php
@@ -3,8 +3,16 @@ require_once __DIR__ . '\..\sdk\zkteco\vendor\autoload.php';
 
 use Jmrashed\Zkteco\Lib\ZKTeco;
 
-$zk = new ZkTeco(ip: "192.168.100.1");
+$zk = new ZkTeco(ip: "192.168.1.203", port: 4370);
 
 $connect = $zk->connect();
 
-echo ($connect) ? "Funka" : "No Funka";
+echo ($connect) ? "Se ha logrado conectar con el biometrico." : "No se ha logrado conectar.";
+try 
+{
+    echo $zk->fmVersion();
+}
+catch(Exception $ex) 
+{
+    echo $ex->getMessage();
+}
