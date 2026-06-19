55adf74a483467c8ea3031b2acbf098d8fd4c74e
Joan Dariel Sánchez Gallegos <joangallegos39@gmail.com>
2026-06-18 23:08:51 -0600

    [cleanup] Depuración de código huerfano

--- MEDIDATA_Lab_Serv/frontend/recursos_humanos/debug_interviews.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../../backend/bd/Conexion.php';
-require '../../backend/registros/rrhh_guard.php';
-
-$pdo = medidata_rrhh_pdo();
-$res = $pdo->query('SELECT * FROM interviews')->fetchAll(PDO::FETCH_ASSOC);
-print_r($res);
